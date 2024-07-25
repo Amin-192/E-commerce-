0135c1ebdf587ba9690a591a3bf03fb40f8e1e0b
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-07-25 22:41:51 +0300

    coming together

--- e-app/src/app/layout/Hero.jsx
+++ e-app/src/app/layout/Hero.jsx
@@ -1,14 +1,80 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 export default function Hero() {
+  const [color,changeColor] = useState('white');
+  const getRandomColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+  const ChangeColor = () => {
+    changeColor(getRandomColor());
+  };
   return (
-    <div className=''>
-      <div className='w-[800px] h-[500px] shadow-2xl shadow-orange-300 
-       left-[27%] absolute top-[18%] '>
+    <div className='' >
+      <button onClick={ChangeColor} style={{ padding: '10px 20px', fontSize: '16px' }}>
+        Change Background Color
+      </button>
+      <div className='w-[900px] h-[800px]  shadow-2xl 
+       left-[27%] absolute top-[18%] 'style={{backgroundColor: color}}>
 
 <div className='p-2'>
   <h1 className='text-8xl text-orange-300 font-bold font-mono
   '>Top <span className='text-red-600'>Deals</span></h1>
+
+</div>
+<div className='items grid grid-cols-3 p-4 gap-5  '>
+
+<div className="card card-compact  w-70 h-[400px] shadow-xl">
+  <figure>
+    <img
+      src="/lowdunk.png"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Nike Low Dunks</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <p className='text-xl text-red-500 relative top-[14%]'>5000 ksh</p>
+      <button className="btn  border-orange-300 border-4 bg-white hover:bg-orange-300">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card card-compact  w-70 h-[400px] shadow-xl">
+  <figure>
+    <img
+      src="/lowdunk.png"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Nike Low Dunks</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <p className='text-xl text-red-500 relative top-[14%]'>5000 ksh</p>
+      <button className="btn  border-orange-300 border-4 bg-white hover:bg-orange-300">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+<div className="card card-compact  w-70 h-[400px] shadow-xl">
+  <figure>
+    <img
+      src="/lowdunk.png"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Nike Low Dunks</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <p className='text-xl text-red-500 relative top-[14%]'>5000 ksh</p>
+      <button className="btn  border-orange-300 border-4 bg-white hover:bg-orange-300">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+
 
 </div>
       </div>
